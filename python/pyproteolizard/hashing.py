e40826f3b60528b43649c78a348fbd581cf4cf07
theGreatHerrLebert <davidteschner@googlemail.com>
2022-03-14 14:48:54 +0100

    comments

--- python/pyproteolizard/hashing.py
+++ python/pyproteolizard/hashing.py
@@ -10,8 +10,16 @@ def bins_to_mz(mz_bin, win_len):
 
 
 class TimsHasher:
-    def __init__(self, trials, len_trial, seed, resolution, num_dalton):
+    """
+    :param trials: number of keys to be generated
+    :param len_trial: bits per key, aka 0 or 1 for every signum created via random projection
+    :param seed: a seed to be able to recreate random hashes for a given parameter setting
+    :param resolution: decimals kept when binning the mz axis of a given mass spectrum
+    :param num_dalton: length of a single window a spectrum is broken into before hashing
+    """
+    def __init__(self, trials, len_trial, seed=5671, resolution=1, num_dalton=10):
 
+        # check
         if 0 < len_trial <= 32:
             self.V = tf.constant(
                 np.expand_dims(np.array([np.power(2, i) for i in range(len_trial)]).astype(np.int32), 1))
@@ -27,21 +35,22 @@ class TimsHasher:
 
         self.trails = trials
         self.len_trial = len_trial
-
         self.seed = seed
         self.resolution = resolution
         self.num_dalton = num_dalton
-
         self.__hash_ptr = pl.TimsHashGenerator(len_trial, trials, seed, resolution, num_dalton)
         self.hash_matrix = self.get_matrix()
         self.hash_tensor = self.tf_tensor()
 
+    # fetch matrix from C++ implementation
     def get_matrix(self):
         return self.__hash_ptr.getMatrixCopy()
 
+    # create TF tensor (potentially GPU located) for hashing
     def tf_tensor(self):
         return tf.transpose(tf.constant(self.hash_matrix.astype(np.float32)))
 
+    # create keys by random projection
     def calculate_keys(self, W):
         # generate signum
         S = (tf.sign(W @ self.hash_tensor) + 1) / 2
@@ -60,31 +69,3 @@ class TimsHasher:
             # calculate int key from binary by base-transform
             H = tf.squeeze(S @ self.V)
             return H
-
-    """
-    def hash_entire_spectrum(self, spectrum: MzSpectrum):
-        return self.__hash_ptr.hashMzSpectrum(spectrum.spec_ptr)
-
-    def hash_spectrum(self, spectrum: MzSpectrum,
-                      min_peaks: int = 5,
-                      min_intensity: int = 150,
-                      window_length: float = 10,
-                      overlapping: bool = False,
-                      restrict: bool = True):
-        bins, hashes = self.__hash_ptr.hashMzSpectrumWindows(spectrum.spec_ptr, min_peaks, min_intensity,
-                                                             window_length, overlapping, restrict)
-
-        return np.array([bins_to_mz(b, window_length) for b in bins]), hashes
-
-    def hash_frame(self, frame: TimsFrame,
-                   min_peaks: int = 5,
-                   min_intensity: int = 150,
-                   window_length: float = 10,
-                   overlapping: bool = False,
-                   restrict: bool = True):
-
-        scans, bins, hashes = self.__hash_ptr.hashTimsFrameWindows(frame.frame_ptr, min_peaks, min_intensity,
-                                                                   window_length, overlapping, restrict)
-
-        return scans, np.array([bins_to_mz(b, window_length) for b in bins]), hashes
-    """
