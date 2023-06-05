2addd12e71b9e6cbda46ee8fa00cb8bcc08646c6
theGreatHerrLebert <davidteschner@googlemail.com>
2023-06-05 10:38:50 +0200

    fixing

--- cpp/src/ParquetTimsDataHandle.cpp
+++ cpp/src/ParquetTimsDataHandle.cpp
@@ -5,12 +5,10 @@
 #include "ParquetTimsDataHandle.h"
 
 #include <sstream>
-#include <arrow/api.h>
 #include <arrow/dataset/api.h>
+#include <arrow/dataset/file_parquet.h>
+#include <arrow/filesystem/localfs.h>
 #include <arrow/io/api.h>
-#include <arrow/result.h>
-#include <parquet/arrow/reader.h>
-#include <parquet/exception.h>
 
 ParquetTimsDataHandle::ParquetTimsDataHandle(std::string dp):
 rawDataPath(initialize(dp) + "raw/"),
@@ -24,20 +22,82 @@ TimsFramePL ParquetTimsDataHandle::getTimsFramePL(int blockId,
     std::stringstream filePath;
     filePath << this->rawDataPath + "BLOCK-" << blockId << ".parquet";
 
-    // Create a ParquetFileReader from a FileSource
-    std::shared_ptr<arrow::io::ReadableFile> infile;
+    // Use the local filesystem
+    auto fs = std::make_shared<arrow::fs::LocalFileSystem>();
 
-    PARQUET_ASSIGN_OR_THROW(
-            infile,
-            arrow::io::ReadableFile::Open(filePath.str()));
-    std::unique_ptr<parquet::arrow::FileReader> reader;
-    PARQUET_THROW_NOT_OK(
-            parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &reader));
+    // Define options
+    arrow::dataset::FileSystemFactoryOptions options;
 
-    // Create a dataset from the ParquetFileReader
-    auto format = std::make_shared<arrow::dataset::ParquetFileFormat>();
-    auto file_source = std::make_shared<arrow::dataset::InMemoryDataset>(reader->parquet_reader());
-    auto dataset = arrow::dataset::Dataset::Make({file_source}, format);
+    // Create a ParquetFileFormat
+    std::shared_ptr<arrow::dataset::ParquetFileFormat> format = std::make_shared<arrow::dataset::ParquetFileFormat>();
+
+    // Create a DatasetFactory
+    std::shared_ptr<arrow::dataset::FileSystemDatasetFactory> factory =
+            std::make_shared<arrow::dataset::FileSystemDatasetFactory>(fs, {filePath}, format, options);
+
+    // Create the Dataset
+    arrow::Result<std::shared_ptr<arrow::dataset::Dataset>> result = factory->Finish();
+
+    /*
+    if (!result.ok()) {
+        // Handle error...
+        return 1;
+    }
+     */
+
+    std::shared_ptr<arrow::dataset::Dataset> dataset = *result;
+
+    // Create a ScannerBuilder
+    std::shared_ptr<arrow::dataset::ScannerBuilder> scanner_builder;
+    auto scanner_builder_result = dataset->NewScan();
+
+    /*
+    if (!scanner_builder_result.ok()) {
+        // Handle error...
+        return 1;
+    }
+     */
+
+    scanner_builder = *scanner_builder_result;
+
+    // Define a predicate
+    auto field = arrow::field("my_field", arrow::int32());  // Adjust this to match a field in your data
+    auto literal = arrow::scalar(10);  // Adjust this to the value you want to filter on
+    auto predicate = arrow::compute::equal(arrow::compute::field_ref(field->name()), literal);
+
+    auto status = scanner_builder->Filter(predicate);
+
+    /*
+    // Apply the predicate
+    if (auto status = scanner_builder->Filter(predicate); !status.ok()) {
+        // Handle error...
+        return 1;
+    }
+     */
+
+    // Create the Scanner
+    arrow::Result<std::shared_ptr<arrow::dataset::Scanner>> scanner_result = scanner_builder->Finish();
+
+    /*
+    if (!scanner_result.ok()) {
+        // Handle error...
+        return 1;
+    }
+     */
+
+    std::shared_ptr<arrow::dataset::Scanner> scanner = *scanner_result;
+
+    // Scan the Dataset into a Table
+    arrow::Result<std::shared_ptr<arrow::Table>> table_result = scanner->ToTable();
+    /*
+    if (!table_result.ok()) {
+        // Handle error...
+        return 1;
+    }
+     */
+    std::shared_ptr<arrow::Table> table = *table_result;
+
+    // Now you can use the Table...
 
     return {};
 }
