6614e50b805c9f0529116b8a9bf0c85583d802d1
theGreatHerrLebert <davidteschner@googlemail.com>
2023-06-05 11:23:06 +0200

    fixing

--- cpp/src/ParquetTimsDataHandle.cpp
+++ cpp/src/ParquetTimsDataHandle.cpp
@@ -5,10 +5,10 @@
 #include "ParquetTimsDataHandle.h"
 
 #include <sstream>
-#include <arrow/dataset/api.h>
-#include <arrow/dataset/file_parquet.h>
-#include <arrow/filesystem/localfs.h>
+#include <arrow/api.h>
 #include <arrow/io/api.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/exception.h>
 
 ParquetTimsDataHandle::ParquetTimsDataHandle(std::string dp):
 rawDataPath(initialize(dp) + "raw/"),
@@ -21,55 +21,6 @@ TimsFramePL ParquetTimsDataHandle::getTimsFramePL(int blockId,
     // construct a path to the parquet file
     std::stringstream filePath;
     filePath << this->rawDataPath + "BLOCK-" << blockId << ".parquet";
-
-    // Use the local filesystem
-    auto fs = std::make_shared<arrow::fs::LocalFileSystem>();
-
-    // Define options
-    arrow::dataset::FileSystemFactoryOptions options;
-
-    // Create a ParquetFileFormat
-    std::shared_ptr<arrow::dataset::ParquetFileFormat> format = std::make_shared<arrow::dataset::ParquetFileFormat>();
-
-    /*
-    // Create a DatasetFactory
-    auto factory = arrow::dataset::FileSystemDatasetFactory::Make(fs, {filePath}, format, options);
-
-    // Create the Dataset
-    arrow::dataset::FinishOptions finish_options;
-
-    auto result = factory->Finish(finish_options);
-
-    std::shared_ptr<arrow::dataset::Dataset> dataset = *result;
-
-    // Create a ScannerBuilder
-    std::shared_ptr<arrow::dataset::ScannerBuilder> scanner_builder;
-
-    auto scanner_builder_result = dataset->NewScan();
-
-    scanner_builder = *scanner_builder_result;
-
-    // Define a predicate
-    auto field = arrow::field("my_field", arrow::int32());  // Adjust this to match a field in your data
-
-    std::shared_ptr<arrow::Scalar> literal;
-
-    auto status = arrow::MakeScalar(10, &literal);
-
-    auto predicate = arrow::compute::equal(arrow::compute::field_ref(field->name()), literal);
-
-    auto status = scanner_builder->Filter(predicate);
-
-    // Create the Scanner
-    arrow::Result<std::shared_ptr<arrow::dataset::Scanner>> scanner_result = scanner_builder->Finish();
-
-    std::shared_ptr<arrow::dataset::Scanner> scanner = *scanner_result;
-
-    // Scan the Dataset into a Table
-    arrow::Result<std::shared_ptr<arrow::Table>> table_result = scanner->ToTable();
-
-    std::shared_ptr<arrow::Table> table = *table_result;
-    */
 
     return {};
 }
